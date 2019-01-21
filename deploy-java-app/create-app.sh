c47887e5cad822fba9ee52b052614fe904a2777f
ahmedmsy <ahmedmsy@amazon.com>
2019-01-21 11:19:01 -0800

    support parallelism of 1

--- deploy-java-app/create-app.sh
+++ deploy-java-app/create-app.sh
@@ -16,6 +16,10 @@ APP_JAR_FILE_NAME=$7
 PARALLELISM=$8
 PARALLELISM_PERKPU=$((PARALLELISM / 2))
 
+if [ "$PARALLELISM_PERKPU" -eq "0" ]; then
+   PARALLELISM_PERKPU=1;
+fi
+
 aws logs create-log-stream --log-group-name java-app-log-group-$TEST_REGION --log-stream-name $APP_NAME-sample-$TEST_REGION-log --region $TEST_REGION 
 sed -e "s/\${i}/1/" -e "s/\#{TEST_REGION}/$TEST_REGION/g;s/\#{PARALLELISM}/$PARALLELISM/g;s/\#{PARALLELISM_PERKPU}/$PARALLELISM_PERKPU/g;s/\#{ACCOUNT_ID}/$ACCOUNT_ID/g;s/\#{BUILT_JAR_S3_BUCKET_ARN}/$BUILT_JAR_S3_BUCKET_ARN/g;s/\#{APP_JAR_FILE_NAME}/$APP_JAR_FILE_NAME/g;s/\#{AWS_ACCOUNT_ROLE_FOR_JAVA_APP}/arn:aws:iam::$3:role\/$4/g;s/\#{APP_NAME}/$APP_NAME/g;s/\#{INPUT_STREAM}/$INPUT_STREAM/g" ./java-app-request.json > deploy-app-$TEST_REGION-$APP_NAME.tmp
 
